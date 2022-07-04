94da500c1a5e9690c27a7436852f21444e0d1e14
Igor Stepin <stepin@stepin.name>
2022-07-04 10:04:51 +0400

    prepare to publish to oss.sonatype.org

---
+++ Module.md
@@ -0,0 +1,14 @@
+# Module Pitest Kotlin plugin
+
+Improves pitest's support for Kotlin.
+
+When the plugin is enabled pitest will avoid creating junk mutations in code that uses
+
+* de structuring code
+* null casts
+* safe casts
+* elvis operator
+
+# Package name.stepin.pitest.kotlin
+
+This package contains all the code for this plugin.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.dokka.gradle.DokkaTask
+import java.net.URL
 
 plugins {
     kotlin("jvm") version "1.7.0"
     `java-library`
     `maven-publish`
+    signing
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0-RC2"
+    id("org.jetbrains.dokka") version "1.7.0"
     jacoco
     id("org.sonarqube") version "3.4.0.2513"
     id("pl.allegro.tech.build.axion-release") version "1.13.14"
@@ -24,6 +29,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compileOnly("org.pitest:pitest:$pitestVersion")
     compileOnly("org.pitest:pitest-entry:$pitestVersion")
+    dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.0")
 
     testImplementation("org.pitest:pitest:$pitestVersion")
     testImplementation("org.pitest:pitest-entry:$pitestVersion")
@@ -55,23 +61,60 @@ java {
     withSourcesJar()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("pitest-kotlin-plugin") {
-            from(components["java"])
-        }
-    }
+val dokkaHtml by tasks.getting(DokkaTask::class)
 
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/stepin/pitest-kotlin")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
+val javadocJar: TaskProvider<Jar> by tasks.registering(Jar::class) {
+    dependsOn(dokkaHtml)
+    archiveClassifier.set("javadoc")
+    from(dokkaHtml.outputDirectory)
+}
+
+publishing {
+    publications.withType<MavenPublication> {
+        artifact(javadocJar)
+        pom {
+            val projectGitUrl = "https://github.com/stepin/pitest-kotlin"
+            name.set(rootProject.name)
+            description.set("Improves pitest's support for Kotlin.")
+            url.set(projectGitUrl)
+            inceptionYear.set("2022")
+            licenses {
+                license {
+                    name.set("Apache License, Version 2.0")
+                    url.set("https://opensource.org/licenses/Apache-2.0")
+                }
+            }
+            developers {
+                developer {
+                    id.set("stepin.name")
+                    name.set("Igor Stepin")
+                    email.set("igor_for_os@stepin.name")
+                    url.set("https://stepin.name")
+                }
+            }
+            issueManagement {
+                system.set("GitHub")
+                url.set("$projectGitUrl/issues")
+            }
+            scm {
+                connection.set("scm:git:$projectGitUrl")
+                developerConnection.set("scm:git:$projectGitUrl")
+                url.set(projectGitUrl)
             }
         }
+        the<SigningExtension>().sign(this)
     }
+    repositories {
+        maven {
+            name = "sonatypeStaging"
+            url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
 }
 
 tasks.test {
@@ -79,4 +122,22 @@ tasks.test {
 }
 tasks.jacocoTestReport {
     dependsOn(tasks.test)
+}
+
+tasks.withType<DokkaTask>().configureEach {
+    dokkaSourceSets {
+        named("main") {
+            moduleName.set("Pitest Kotlin plugin")
+            includes.from("Module.md")
+            sourceLink {
+                localDirectory.set(file("src/main/kotlin"))
+                remoteUrl.set(
+                    URL(
+                        "https://github.com/stepin/pitest-kotlin/tree/main/src/main/kotlin"
+                    )
+                )
+                remoteLineSuffix.set("#L")
+            }
+        }
+    }
 }
